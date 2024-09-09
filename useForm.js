cff8b6809a7aca214bb3096e375ae74c214a2396
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-09 11:56:21 -0400

    primer commit

---
+++ useForm.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export const useForm = (initialForm = {}) => {
+    
+    const [formState, setFormState] = useState(initialForm);
+    
+    const onInputChange = ({target}) => {
+        const {value, name} = target;
+        //console.log({name, value})
+        setFormState(
+            {
+                ...formState,
+                [name]: value
+            }
+        )
+    }
+    const onResetForm = () => {
+        setFormState(initialForm)
+    }
+
+
+    return {
+        formState,
+        ...formState,
+        onInputChange,
+        onResetForm
+    }
+}
